9ff59fdb4989de63ab7c4d67d89c32ebc0cad320
eirillsh <eirill.strand.hauge@gmail.com>
2018-09-09 18:46:55 +0200

    testing

--- code/test.cpp
+++ code/test.cpp
@@ -60,9 +60,12 @@ int main() {
 		printf("\t- FAILED : max value did not match expected max value\n\n");
 	}
 
+	delete[] myArray;
+	delete[] otherArray;
+
 	printf("\nvoid generalTriDiaSolver(double *a, double *b, double *c, double *v, double *f, int n)\n");
 
-	n = 20;
+	n = 100;
 	double *a, *b, *c, *x, *v, *f; 
 	srand(time(NULL));
 	v = createVector(0, n);
@@ -71,7 +74,7 @@ int main() {
 	a = createVector(0, n-1);
 	b = createVector(0, n);
 	c = createVector(0, n-1);
-	float multiplier = 1000;
+	double multiplier = 100;
 
 	for (int i = 0; i < (n - 1); i++) {
 		x[i] = rand()/double(RAND_MAX)*multiplier;
@@ -91,31 +94,63 @@ int main() {
 
 	generalTriDiaSolver(a, b, c, v, f, n);
 
-	success = true;
-	eps = 1e-12;
-
 	double max_error = fabs(v[0] - x[0]);
 	double max_relError = fabs(v[0] - x[0])/x[0];
 	double i_error, i_relError;
 	for (int i = 1; i < n; i++) {
 		i_error = fabs(v[i] - x[i]);
 		i_relError = fabs(v[i] - x[i])/x[i];
-		if (fabs(v[i] - x[i]) > ma) {
-			success = false;
-			break;
+		if (i_error > max_error) {
+			max_error = i_error;
+		}
+		if (i_relError > max_relError) {
+			max_relError = i_relError;
 		}
 	}
-	if (success) {
-		printf("\t- PASSED : calculated array v seems correct\n\n");
-	} else {
-		printf("\t- FAILED : calculated array v does not match expected\n\n");
+	printf("\t-max error = %.2g\n\t-max relative error = %.2g\n\n", max_error, max_relError);
+
+	//for (int i = 0; i < n; i++) {printf("%.30f\t%.30f\n", v[i], x[i]);}
+	delete[] a;
+	delete[] b;
+	delete[] c;
+	delete[] f;
+	delete[] v;
+
+
+	printf("\nvoid constTriDiaSolver(double a, double b, double c, double *v, double *f, int n)\n");
+	double a_value = rand()/double(RAND_MAX)*multiplier;
+	double b_value = rand()/double(RAND_MAX)*multiplier;
+	double c_value = rand()/double(RAND_MAX)*multiplier;
+	printf("%.15f\t%.15f\t%.15f\n", a_value, b_value, c_value);
+	double *u, *g;
+	u = createVector(0, n);
+	g = createVector(0, n);
+
+
+	g[0] = x[0]*b_value + x[1]*c_value;
+	for (int i = 1; i < (n - 1); i++) {
+		g[i] = a_value*x[i-1] + b_value*x[i] + c_value*x[i+1];
 	}
+	g[n-1] = a_value*x[n-2] + b_value*x[n-1];
 
-	for (int i = 0; i < n; i++) {printf("%.30f\n", fabs(v[i] - x[i]));}
+	constTriDiaSolver(a_value, b_value, c_value, u, g, n);
 
-
-	printf("\nvoid constTriDiaSolver(double a, double c, double *b, double *v, double *f, int n)\n");
-
+	max_error = fabs(u[0] - x[0]);
+	max_relError = fabs(u[0] - x[0])/x[0];
+	for (int i = 1; i < n; i++) {
+		i_error = fabs(u[i] - x[i]);
+		i_relError = fabs(u[i] - x[i])/x[i];
+		if (i_error > max_error) {
+			max_error = i_error;
+		}
+		if (i_relError > max_relError) {
+			max_relError = i_relError;
+		}
+	}
+	printf("\t-max error = %.2g\n\t-max relative error = %.2g\n\n", max_error, max_relError);
+	for (int i = 0; i < n; i++) {printf("%.15f\t%.15f\t%.15f\n", u[i], x[i], g[i]);}
+	delete[] u;
+	delete[] g;
 
 	return 0;
 }
