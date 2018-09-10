eb8f97a5fc3ac1d1aeb27a4b5cfcd32cf20bb0f3
Anders Julton <31956888+andersjulton@users.noreply.github.com>
2018-09-10 13:40:47 +0200

    Added loglog plot to Project_1.py

--- Project1/Project_1.py
+++ Project1/Project_1.py
@@ -25,6 +25,7 @@ x_100 = np.linspace(0, 1, n_100)
 x_1000 = np.linspace(0, 1, n_1000)
 u = 1 - (1 - np.exp(-10))*x_1000 - np.exp(-10*x_1000)
 
+plt.figure()
 plt.plot(x_1000, u, label="Exact solution")
 plt.plot(x_10, v_10, label="n = 10")
 plt.plot(x_100, v_100, label="n = 100")
@@ -34,4 +35,15 @@ plt.legend(loc="best", fontsize=12)
 plt.xlabel("x", fontsize=12)
 plt.ylabel("u(x)", fontsize=12)
 plt.savefig("compare.png")
+plt.show()
+
+eps = [-1.1797, -3.0880, -5.08, -7.0792, -6.5691, -4.6313, -2.7325]
+n = np.array([10, 100, 1000, 10e4, 10e5, 10e6, 10e7])
+h = 1/(n+1)
+
+plt.figure()
+plt.loglog(h, np.exp(eps), '^')
+plt.xlabel(r'$h$', fontsize=15)
+plt.ylabel(r'$\epsilon_{max}(h)$', fontsize=15)
+plt.savefig('log_plot.png')
 plt.show()
