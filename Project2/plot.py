af057527a151e378e56aeead36855cfbef5a732c
Jultis <31956888+Jultis@users.noreply.github.com>
2018-09-30 18:11:00 +0200

    Made main runable

--- Project2/plot.py
+++ Project2/plot.py
@@ -2,35 +2,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('TkAgg')
 
-with open("wave_21.txt", "r") as infile:
+with open("wave_1.txt", "r") as infile:
     n = int(infile.readline())
     u1 = np.zeros(n)
     for i in range(n):
         u1[i] = infile.readline()
 
-with open("wave_22.txt", "r") as infile:
+with open("wave_2.txt", "r") as infile:
     n = int(infile.readline())
     u2 = np.zeros(n)
     for i in range(n):
         u2[i] = infile.readline()
 
-with open("wave_23.txt", "r") as infile:
+with open("wave_3.txt", "r") as infile:
     n = int(infile.readline())
     u3 = np.zeros(n)
     for i in range(n):
         u3[i] = infile.readline()
 
-with open("wave_24.txt", "r") as infile:
+with open("wave_4.txt", "r") as infile:
     n = int(infile.readline())
     u4 = np.zeros(n)
     for i in range(n):
         u4[i] = infile.readline()
+with open("wave_1n.txt", "r") as infile:
+    n = int(infile.readline())
+    u1n = np.zeros(n)
+    for i in range(n):
+        u1n[i] = infile.readline()
 
-x = np.linspace(0, 1, n)
+with open("wave_2n.txt", "r") as infile:
+    n = int(infile.readline())
+    u2n = np.zeros(n)
+    for i in range(n):
+        u2n[i] = infile.readline()
 
-plt.figure()
-plt.plot(x, u1, color='red')
-plt.plot(x, u2, color='green')
-plt.plot(x, u3, color='blue')
-plt.plot(x, u4, color='orange')
+with open("wave_3n.txt", "r") as infile:
+    n = int(infile.readline())
+    u3n = np.zeros(n)
+    for i in range(n):
+        u3n[i] = infile.readline()
+
+with open("wave_4n.txt", "r") as infile:
+    n = int(infile.readline())
+    u4n = np.zeros(n)
+    for i in range(n):
+        u4n[i] = infile.readline()
+
+x = np.linspace(0, 5, n)
+
+plt.figure(figsize=(20,15))
+plt.subplot(121)
+plt.plot(x, u1, color='red', label=r'$\omega_r = 0.01$')
+plt.plot(x, u2, color='green', label=r'$\omega_r = 0.5$')
+plt.plot(x, u3, color='blue', label=r'$\omega_r = 1$')
+plt.plot(x, u4, color='orange', label=r'$\omega_r = 5$')
+plt.ylabel(r'$\psi_0(\rho)$')
+plt.xlabel(r'$\rho$')
+plt.legend(loc='best')
+plt.subplot(122)
+plt.plot(x, u1n, color='red', label=r'$\omega_r = 0.01$')
+plt.plot(x, u2n, color='green', label=r'$\omega_r = 0.5$')
+plt.plot(x, u3n, color='blue', label=r'$\omega_r = 1$')
+plt.plot(x, u4n, color='orange', label=r'$\omega_r = 5$')
+plt.ylabel(r'$\psi_0(\rho)$')
+plt.xlabel(r'$\rho$')
+plt.legend(loc='best')
+#plt.savefig('two_electrons.png')
 plt.show()
--- Project2/utils.h
+++ Project2/utils.h
@@ -20,10 +20,14 @@ double maxEpsilon(double *expected, double *computed, int n);
 
 void printError(double *u, double *v, int n);
 
-void arrayToFile(double *v , int n, std::string filename);
+void arrayToFile(double *v , int n, std::string filename, bool zeroPadding = false);
 
 void sortEig(double *eigval, double **eigvec, int n);
 
 double **transpose(double **A, int n);
 
 double analyticConvergenceRate(int n, double eps, double sumOff);
+
+void normalize(double *v, double *u, int n);
+
+void extractEigenVec(double **A, double *u, int index, int n);
