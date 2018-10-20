c596b3cb83c4bb59accd8657987a3bd9266bf288
Anders Julton <31956888+andersjulton@users.noreply.github.com>
2018-10-20 11:16:55 +0200

    Update Project3/plot3D.py

--- Project3/plot3D.py
+++ Project3/plot3D.py
@@ -3,7 +3,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 
-def read_file(filename):
+def readFileMatrix(filename):
 	with open(filename, "r") as infile:
 		line_1 = infile.readline()
 		n = int(line_1.split()[0])
@@ -15,16 +15,33 @@ def read_file(filename):
 				v[i][j] = float(line.split()[j])
 	return v
 
-pos_x = read_file("pos_x.txt")
-pos_y = read_file("pos_y.txt")
-pos_z = read_file("pos_z.txt")
+def readFileArray(filename):
+	with open(filename, "r") as infile:
+		n = int(infile.readline().strip())
+		v = np.zeros(n)
+		for i in range(n):
+			v[i] = float(infile.readline().strip())
+	return v
 
-names = ["Sun", "Mercury", "Venus", "Earth"]#, "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
+def plotMany():
+	names = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+	for i, n in enumerate(names):
+		ax.plot(pos_x[:,i], pos_y[:,i], pos_z[:,i], label = n, linewidth=0.5)
+	ax.legend()
+	plt.show()
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-#ax.set_zlim(-1, 1)
-for i, n in enumerate(names):
-	ax.plot(pos_x[:,i], pos_y[:,i], pos_z[:,i], label = n, linewidth=0.5)
-ax.legend()
+def plotOne(planet):
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+	ax.plot(pos_x, pos_y, pos_z, label = planet, linewidth=0.5)
+	ax.legend()
+	plt.show()
+
+pos_x = readFileArray("ES_pos_x.txt")
+pos_y = readFileArray("ES_pos_y.txt")
+
+
+plt.plot(pos_x[0:100], pos_y[0:100])
 plt.show()
