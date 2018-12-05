83943864dd5e73d45f2edf770f3fc235c4d04664
Anders Julton <31956888+andersjulton@users.noreply.github.com>
2018-12-05 11:07:28 +0100

    anal work in progress

--- Project5/diffusion_exact.cpp
+++ Project5/diffusion_exact.cpp
@@ -1,4 +1,24 @@
+#define _USE_MATH_DEFINES
+
 #include "utils.h"
+#include <cmath>
+
+double u(double x, double t, double L, int n) {
+    double val = 0;
+    for(int i = 1; i < n; i++) {
+        val += pow(-1, i+1)*2*L/(M_PI*n)*sin(i*M_PI*x/L)*exp(-pow(i*M_PI, 2)*t)
+    }
+    return x - val;
+}
+
+double *exact(double *v, double x0, double x1, double m, double t, double L, int n) {
+    double dx = (x1 - x0)/m;
+    double x;
+    for(int i = 0; i < (m+1); i++) {
+        x = x0 + i*dx
+        v[i] = u(x, t, L, n);
+    }
+}
 
 
-// ALL YOURS
+// NOT DONE
